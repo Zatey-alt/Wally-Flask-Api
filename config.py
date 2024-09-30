c3e46b2b5d063deb7536653182c5d197aa071247
Zatey <115568968+Zatey-alt@users.noreply.github.com>
2024-09-30 11:25:52 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = '6F6598821C1485A99C499C8EE2859'
     UPLOAD_FOLDER = '/static/uploads/'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-    SQLALCHEMY_DATABASE_URI = 'postgresql://wally_database_user:W8ZiNLr3Gb5SkYV1GxBSxbuIFiOaeNrv@dpg-crt7hoo8fa8c73ct1d0g-a.oregon-postgres.render.com/wally_database'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://wally_database_6ll1_user:gWVOkr9oL5Cv4k5W19m9SVIRth3hl2oc@dpg-crt8l75umphs73fkf250-a.oregon-postgres.render.com/wally_database_6ll1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
